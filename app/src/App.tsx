79b3cc592ca907aa29b90d0471f4321c8ae1749b
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-13 15:29:19 -0300

    feat: add axios and table mantine

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,17 @@
 // import { useState } from 'react'
 
 import ChannelScreen from "./modules"
+import { MantineProvider } from "@mantine/core";
+
 
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <ChannelScreen></ChannelScreen>
+    <MantineProvider>
+      <ChannelScreen></ChannelScreen>
+    </MantineProvider>
   )
 }
 
