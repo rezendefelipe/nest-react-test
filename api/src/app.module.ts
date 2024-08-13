6de7251344d7ea5cc2bf9407c63057faa7ebd1f5
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-13 10:07:45 -0300

    feat: add channel service and controller

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChannelController } from './channel/channel.controller';
+import { ChannelService } from './channel/channel.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ChannelController],
+  providers: [AppService, ChannelService],
 })
 export class AppModule {}
