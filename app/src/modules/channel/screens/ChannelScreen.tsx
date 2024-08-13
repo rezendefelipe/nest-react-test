0c71e430176ac90b8a8d78c666cd7e3fe6762943
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-13 16:49:56 -0300

    feat: add icons actions on each row

--- app/src/modules/channel/screens/ChannelScreen.tsx
+++ app/src/modules/channel/screens/ChannelScreen.tsx
@@ -1,9 +1,10 @@
-import { Table } from "@mantine/core";
+import { ActionIcon, Table } from "@mantine/core";
 
 import { URL_ALL_CHANNELS } from "../../../shared/constants/urls.ts";
 import { useRequests } from "../../../shared/hooks/useRequests.ts";
 import { useEffect, useState } from "react";
 import { ChannelType } from "../types/ChannelType.ts";
+import { IconTrashFilled, IconEdit } from '@tabler/icons-react';
 
 
 const ChannelScreen = () => {
@@ -16,8 +17,17 @@ const ChannelScreen = () => {
       if (resp) {
         const rows = resp.map((element: ChannelType) => (
           <Table.Tr key={element.id}>
+            <Table.Td>{element.id}</Table.Td>
             <Table.Td>{element.name}</Table.Td>
             <Table.Td>{element.description}</Table.Td>
+            <Table.Td>
+              <ActionIcon variant="light" color="red" aria-label="Settings">
+                <IconTrashFilled style={{ width: '70%', height: '70%' }} stroke={1.5} />
+              </ActionIcon>
+              <ActionIcon variant="light" aria-label="Settings">
+                <IconEdit style={{ width: '70%', height: '70%' }} stroke={1.5} />
+              </ActionIcon>
+            </Table.Td>
           </Table.Tr>
         ));
 
@@ -29,12 +39,13 @@ const ChannelScreen = () => {
 
   return (
     <div>
-      <Table>
+      <Table striped highlightOnHover withTableBorder withColumnBorders>
         <Table.Thead>
           <Table.Tr>
             <Table.Th>Id</Table.Th>
             <Table.Th>Channel Name</Table.Th>
             <Table.Th>Channel Description</Table.Th>
+            <Table.Th>Actions</Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{data}</Table.Tbody>
