c560505596da526fb6922eafe0f07d34bdd7ec8e
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-14 01:35:19 -0300

    fix: adjust msg error

--- app/src/shared/constants/errorStatus.ts
+++ app/src/shared/constants/errorStatus.ts
@@ -1,3 +1,2 @@
-export const ERROR_ACCESS_DENIED = 'Sem permissão.';
-export const ERROR_CONNECTION = 'erro connetion.';
-export const ERROR_AUTH = 'user or password invelid.';
+export const ERROR_ACCESS_DENIED = 'Forbidden.';
+export const ERROR_CONNECTION = 'Error Connetion.';
