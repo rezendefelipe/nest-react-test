a7614f04f2abcfc1f984da0f65354ac878028f8b
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-14 00:24:20 -0300

    fix: adjust menu header

--- app/src/shared/components/header.tsx
+++ app/src/shared/components/header.tsx
@@ -4,10 +4,7 @@ import { IconSearch } from '@tabler/icons-react';
 import classes from '../styles/header.module.css';
 
 const links = [
-  { link: '/about', label: 'Features' },
-  { link: '/pricing', label: 'Pricing' },
-  { link: '/learn', label: 'Learn' },
-  { link: '/community', label: 'Community' },
+  { link: '/', label: 'Home' },
 ];
 
 export function HeaderComponent() {
@@ -18,7 +15,6 @@ export function HeaderComponent() {
       key={link.label}
       href={link.link}
       className={classes.link}
-      onClick={(event) => event.preventDefault()}
     >
       {link.label}
     </a>
@@ -33,9 +29,9 @@ export function HeaderComponent() {
         </Group>
 
         <Group>
-          {/* <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
+          <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
             {items}
-          </Group> */}
+          </Group>
           <Autocomplete
             className={classes.search}
             placeholder="Search"
